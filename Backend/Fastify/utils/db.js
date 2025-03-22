b268090f8c5ac04484c9938c87277b8aac398515
justeozan <justeoz.an+projects@gmail.com>
2025-03-22 12:10:31 +0100

    cleaning

--- Backend/Fastify/utils/db.js
+++ Backend/Fastify/utils/db.js
@@ -4,23 +4,17 @@ import { CREATE_USERS_TABLE } from '../models/userModel.js';
 import { CREATE_BLACKLIST_ACCESS_TOKENS_TABLE, CREATE_BLACKLIST_REFRESH_TOKENS_TABLE } from "../models/tokenModel.js";
 
 const dbFile = "Data/db/database.sqlite";
-// const passphrase = "votre-mot-de-passe-securise"; // Remplacez ceci par un mot de passe sécurisé
-// const dbFile = path.resolve()
 
 async function setupDatabase() {
 	try {
 		const { user, pass } = await getSQLiteCreds()
-		
 		const db = new Database(dbFile, {
 			verbose: null,
 			fileMustExist: false
 		})
 
-		// db.pragma(`key = 'test'`)
-		// db.pragma(`user_key = '${user}'`)
+		db.pragma(`user_key = '${user}'`)
 		db.pragma(`key = '${pass}'`)
-		console.log(`Connected to SQLite as ${user}`)
-	
 		return db
 	} catch (err) {
 		console.error("Error config of SQLite:", err)
@@ -28,11 +22,10 @@ async function setupDatabase() {
 	}
 }
 
-const db = await setupDatabase();
-
 export function initDb() {
 	db.prepare(CREATE_USERS_TABLE).run();
 	console.log("\n✅ Base de données initialisée !");
 }
 
+const db = await setupDatabase();
 export default db;
